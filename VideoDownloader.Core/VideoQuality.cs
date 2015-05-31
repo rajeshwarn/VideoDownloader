673dd1dfe51a4b98b542a5656e68680f52c5fa01
yl10030270 <yangliang10030270@gmail.com>
2015-05-31 12:56:27 -0700

    add file

---
+++ VideoDownloader.Core/VideoQuality.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace VideoDownloader.Core
+{
+    public class VideoQuality
+    {
+        public Extentisons Extention { get; set; }
+        public Dimension Dimension { get; set; }
+        public string DownloadUrl { get; set; }
+        public long FileSize { get; set; }
+
+        public override string ToString()
+        {
+            //var test = string.Format()
+            var videoSize = String.Format(new FileSizeFormatProvider(), "{0:fs}", FileSize);
+            return String.Format("{0} ({1}) - {2}", Extention, Dimension, videoSize);
+        }
+    }
+}
